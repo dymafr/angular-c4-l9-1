62eb605189f2ce8eb9543be0f2b814c63eb7b6c0
dymafr <erwan1309@hotmail.com>
2021-09-07 12:35:48 +0200

    2 files modified

--- src/enfant.component.ts
+++ src/enfant.component.ts
@@ -1,31 +1,28 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-enfant',
-  
+
   template: `
-    <div *ngFor="let log of changeLog;">{{log}}</div>
+    <div *ngFor="let log of changeLog">{{ log }}</div>
   `
 })
-
 export class EnfantComponent implements OnChanges {
-  
-  @Input() prenom;
-  @Input() monObj;
-  
+  @Input() prenom?: string;
+  @Input() monObj?: { prenom: string };
+
   public changeLog = [];
-  
-  constructor() { }
 
-  ngOnChanges(changes) {
-    
-    for (let prop in changes) {
-      let chng = changes[prop];
-      let cur  = JSON.stringify(chng.currentValue);
-      let prev = JSON.stringify(chng.previousValue);
-      this.changeLog.push(`${prop}: valeurActuelle = ${cur}, valeurPrécédente = ${prev}`);
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const prop in changes) {
+      const chng = changes[prop];
+      const cur = JSON.stringify(chng.currentValue);
+      const prev = JSON.stringify(chng.previousValue);
+      this.changeLog.push(
+        `${prop}: valeurActuelle = ${cur}, valeurPrécédente = ${prev}`
+      );
     }
-    
   }
-
 }
